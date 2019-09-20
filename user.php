158cf8bdf921e0e6ebf42b15a25801da86c9f86c
Mike <mnicholas@albany.edu>
2019-09-20 00:15:37 -0400

    Add files via upload
    Created rudimentary landing page, with drop down menu and path to create a new portfolio (which currently leads nowhere).

---
+++ user.php
@@ -0,0 +1,49 @@
+<!--
+CSI 418
+Modified from my CS410 - Intro to Databases Final Project
+    
+    This script creates the user landing page. The landing page
+    contains links to all of the user functions, listed below. 
+<img src="plus.png" height="240" width="320">
+<?php include "templates/header.php"; ?>
+
+-- takes user to page to add a portfolio -- 
+    Create a new portfolio here!
+    <a href="addPortfolio.php"><img src="plus.png"></a>
+
+
+<?php include "templates/footer.php"; ?>
+    -->
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="userStyle.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+
+<!--https://stackoverflow.com/questions/11651366/php-pass-data-to-html-form  -->
+<?php
+$uname = $_GET['uname'];
+?>     
+<div class="dropdown">
+  <button class="dropbtn">My Profile - <?php echo $uname ?></button>
+  <div class="dropdown-content">
+    <a href="#">Account Settings</a>
+    <a href="#">Portfolios</a>
+    <a href="#">Support</a>
+    <a href="access.html">Log Out</a>
+  </div>
+</div>
+<div class="container">
+    
+    <div class="panel panel-default">
+        <div class="panel-body"><a href="#"><img src="plusText.png" /></a></div>
+    </div>
+</div>
+
+
+</body>
+</html>
+
