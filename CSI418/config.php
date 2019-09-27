1b28a396298ce771229f369d536efd46c586bfd6
Mike <mnicholas@albany.edu>
2019-09-27 13:50:10 -0400

    Database Initialization File, Change to Config
    When the contents of this file are dragged into wamp's mysql console, the database is created and initialized with user, portfolio, and stock tables. The latter two tables I was unsure as to what information we wanted to have in those, so I left them to be decided.
    I changed the config file to reflect the new name I gave for the database, "TRADEX_DATABASE".

--- CSI418/config.php
+++ CSI418/config.php
@@ -6,8 +6,8 @@ Modified from my CS410 - Intro to Databases Final Project
 
 $host       = "localhost";
 $username   = "root";
-$password   = "mike6151"; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
-$dbname     = "portfolio-database"; // will use later
+$password   = ""; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
+$dbname     = "TRADEX_DATABASE"; // will use later
 $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
