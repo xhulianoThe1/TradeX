9776f1944df10dff77fb45ea795543e170eaed28
vincentballarano <vballarano@albany.edu>
2019-10-10 18:55:22 -0400

    new

--- CSI418/config.php
+++ CSI418/config.php
@@ -4,11 +4,13 @@
 Modified from my CS410 - Intro to Databases Final Project
   */
 
-$host       = "localhost";
-$username   = "root";
-$password   = ""; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
+$host       = "us-cdbr-iron-east-05.cleardb.net";
+$username   = "bb08ccb364f632";
+$password   = "cd4d0490"; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
 $dbname     = "TRADEX_DATABASE"; // will use later
 $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
+
+//mysql://bb08ccb364f632:cd4d0490@us-cdbr-iron-east-05.cleardb.net/heroku_6c090cb7cb60414?reconnect=true
