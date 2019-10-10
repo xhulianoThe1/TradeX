dac8c5b2596682a98fbf69771017c6a0fff24027
vincentballarano <vballarano@albany.edu>
2019-10-10 18:07:01 -0400

    make it better

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var server = http.Server(app);
 app.use(express.static('CSI418'));
 
 server.listen(PORT, function() {
-  console.log('Chat server running');
+  console.log('server running');
 });
 
 var io = require('socket.io')(server);
