a94ccb4d79822565fa7dbd5aa7073b723dfddfb2
Michael Nicholas <mnicholas@albany.edu>
2019-10-11 18:53:45 -0400

    change js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
@@ -18,3 +19,34 @@ io.on('connection', function(socket) {
     io.emit('message', msg);
   });
 });
+*/
+
+const port = 3000;
+
+var express = require('express'),
+    app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.render('form');// if jade
+  // You should use one of line depending on type of frontend you are with
+  res.sendFile(__dirname + '/form.html'); //if html file is root directory
+ res.sendFile("index.html"); //if html file is within public directory
+});
+
+app.post('/',function(req,res){
+   var username = req.body.username;
+   var htmlData = 'Hello:' + username;
+   res.send(htmlData);
+   console.log(htmlData);
+});
+
+app.listen(port);
