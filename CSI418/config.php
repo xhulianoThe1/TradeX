59c3134e52731fd10f4e1a52b02d249a651b5a11
Michael Nicholas <mnicholas@albany.edu>
2019-10-11 18:12:45 -0400

    mike test

--- CSI418/config.php
+++ CSI418/config.php
@@ -5,12 +5,12 @@ Modified from my CS410 - Intro to Databases Final Project
   */
 
 $host       = "us-cdbr-iron-east-05.cleardb.net";
-$username   = "bb08ccb364f632";
-$password   = "cd4d0490"; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
-$dbname     = "heroku_6c090cb7cb60414e"; // will use later
+$username   = "bd9acdd9020e4f";
+$password   = "c31a396a"; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
+$dbname     = "heroku_bf9276ba3a568af"; // will use later
 $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
 
-//mysql://bb08ccb364f632:cd4d0490@us-cdbr-iron-east-05.cleardb.net/heroku_6c090cb7cb60414?reconnect=true
+//mysql://bd9acdd9020e4f:c31a396a@us-cdbr-iron-east-05.cleardb.net/heroku_bf9276ba3a568af?reconnect=true
