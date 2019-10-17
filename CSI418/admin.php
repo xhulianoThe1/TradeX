ddc69710f90d9872d13935a49632ed0cebc8620c
vincentballarano <vballarano@albany.edu>
2019-10-16 22:35:39 -0400

    made an iffy change to admin

--- CSI418/admin.php
+++ CSI418/admin.php
@@ -3,14 +3,19 @@
     
     This script creates the administrative landing page. It contains links to all of the 
     Administrative functions, listed below-->
-
+if(!isset($_POST['submit']){
+	header('Location: index.php');
+	exit();
+}else{
 <?php include "templates/header.php"; ?>
 
 <ul>
   <li>
+  
       <a href="doesntexistyet"><strong>You're on the admin page! This link doesn't go anywhere!</strong></a>
   </li>
 
 </ul>
 
 <?php include "templates/footer.php"; ?>
+}
