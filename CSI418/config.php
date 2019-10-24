9e425e231ad6b21f58a2a2fd05dcb53deb3387e8
Michael Nicholas <mnicholas@albany.edu>
2019-10-24 15:49:32 -0400

    updated config to new database for tradexassets

--- CSI418/config.php
+++ CSI418/config.php
@@ -15,14 +15,14 @@ $options    = array(
               );
 
 */
-//mysql://bd9acdd9020e4f:c31a396a@us-cdbr-iron-east-05.cleardb.net/heroku_bf9276ba3a568af?reconnect=true
+//mysql://bb874a893dce20:84f548c8@us-cdbr-iron-east-05.cleardb.net/heroku_a6afaaba3422b9a?reconnect=true
 
 
 
 $host       = "us-cdbr-iron-east-05.cleardb.net";
-$username   = "bd9acdd9020e4f";
-$password   = "c31a396a"; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
-$dbname     = "heroku_bf9276ba3a568af"; // will use later
+$username   = "bb874a893dce20";
+$password   = "84f548c8"; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
+$dbname     = "heroku_a6afaaba3422b9a"; // will use later
 $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
