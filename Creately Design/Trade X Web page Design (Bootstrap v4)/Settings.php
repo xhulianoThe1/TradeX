bc6ed6ff5df171ecb3af033196222f2468b0826c
Mike W <54906677+MichaelWoodmansee@users.noreply.github.com>
2019-10-31 15:04:07 -0400

    Add files via upload

---
+++ Creately Design/Trade X Web page Design (Bootstrap v4)/Settings.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Trade X</title>
+  <!--Bootstrap v4 -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark" id ="nav">
+  <a class="navbar-brand" href="#">Trade X</a>
+   <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+   <!--End ofn Toggler/collapsibe Button -->  
+    
+    <!-- Navbar links -->
+    <ul class="navbar-nav">
+      <!--Landing Page link on the navbar-->
+      <li class="nav-item">
+        <a class="nav-link" href="Landing.html">Landing Page</a>
+      </li>
+      <!--Viewer link on the navbar-->
+      <li class="nav-item">
+        <a class="nav-link" href="Viewer.html">Viewer</a>
+      </li>
+      <!--User Maunal link on the navbar-->
+      <li class="nav-item">
+        <a class="nav-link" href="UserManual.html">User Manual</a>
+      </li>
+      <!--Sign-out link-->  
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sign-out</a>
+      </li>
+    </ul>
+    <!--End of Navbar links -->
+  </div>  
+</nav>
+
+    <!--Content Div's-->
+    
+
+</body>
+</html>
---
+++ Creately Design/Trade X Web page Design (Bootstrap v4)/Viewer.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Trade X</title>
+  <!--Bootstrap v4 -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark" >
+  <a class="navbar-brand" href="#">Trade X</a>
+   <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+   <!--End ofn Toggler/collapsibe Button -->  
+    
+    <!-- Navbar links -->
+    <ul class="navbar-nav">
+      <!-- landing Page link -->
+      <li class="nav-item">
+        <a class="nav-link" href="Landing.html">Landing Page</a>
+      </li>
+      <!-- User Manual link -->
+      <li class="nav-item">
+        <a class="nav-link" href="UserManual.html">User Manual</a>
+      </li>
+      <!-- Settings link-->
+      <li class="nav-item">
+        <a class="nav-link" href="Settings.html">Settings</a>
+      </li>
+      <!--Sign-out link-->  
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sign-out</a>
+      </li>
+    </ul>
+    <!--End of Navbar links -->
+  </div>  
+</nav>
+
+    <!--Content Div's-->
+   
+</body>
+</html>
