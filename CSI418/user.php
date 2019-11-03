15bd40a3fc79b8588f78804715f7169ae798fbf8
vincentballarano <vballarano@albany.edu>
2019-11-03 13:28:52 -0500

    testing simple highchart

--- CSI418/user.php
+++ CSI418/user.php
@@ -132,7 +132,7 @@ body{
         <h1></h1>
     
     <div class="panel panel-default">
-        <div class="panel-body"><a href="highcharts/index.php"><img src="plusText.png" alt="add portfolio image" /></a></div>
+        <div class="panel-body"><a href="highchartsTest.php"><img src="plusText.png" alt="add portfolio image" /></a></div>
     </div>
     </div>
 <!--
