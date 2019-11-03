c00942e6bab3b28982b1cef57de671ec2f89cc24
Michael Nicholas <mnicholas@albany.edu>
2019-11-02 21:45:32 -0400

    removed duplicate code from updating github

--- CSI418/config.php
+++ CSI418/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 /** CSI 418
@@ -28,34 +27,4 @@ $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
-=======
-<?php
-
-/** CSI 418
-Modified from my CS410 - Intro to Databases Final Project
-  */
-
-/*
-$host       = "localhost";
-$username   = "root";
-$password   = "mike6151"; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
-$dbname     = "TRADEX_DATABASE"; // will use later
-$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
-$options    = array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-              );
-
-*/
-//mysql://bb874a893dce20:84f548c8@us-cdbr-iron-east-05.cleardb.net/heroku_a6afaaba3422b9a?reconnect=true
-
-
-
-$host       = "us-cdbr-iron-east-05.cleardb.net";
-$username   = "bb874a893dce20";
-$password   = "84f548c8"; // this password will be changed when the database is uploaded to a webhosting service, for now its configured to the password of the local machine
-$dbname     = "heroku_a6afaaba3422b9a"; // will use later
-$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
-$options    = array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-              );
->>>>>>> 3760ce4551e02755a513a2ff1e00f7664e92f11c
+?>
--- CSI418/inactiveLogout.php
+++ CSI418/inactiveLogout.php
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-<?php
-//retrieved from https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
-
-// Initialize the session
-session_start();
-
-// Unset all of the session variables
-$_SESSION = array();
- 
-// Destroy the session.
-session_destroy();
-
-session_start();
-
-$_SESSION['inactive'] = true;
-
-// Redirect to login page
-header("location: index.php");
-exit;
-
-?>
-=======
 <?php
 //retrieved from https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
 
@@ -42,4 +19,4 @@ header("location: index.php");
 exit;
 
 ?>
->>>>>>> 3760ce4551e02755a513a2ff1e00f7664e92f11c
+
