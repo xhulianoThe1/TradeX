51c4e76f448395b19155f2ac92ae917c1c08ace3
vincentballarano <vballarano@albany.edu>
2019-11-03 15:55:59 -0500

    testing simple highchart

--- CSI418/highchartsTest.php
+++ CSI418/highchartsTest.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Highcharts using mysql php- parsing JSON data</title>
+	<title>Testing highcharts</title>
 	<script
   src="http://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
@@ -11,32 +11,37 @@
 <body>
 <div id="container" style="width:100%; height:400px;"></div>
 
-<script type="test/javascript">
-document.addEventListener('DOMContentLoaded', function () {
-        var myChart = Highcharts.chart('container', {
-            chart: {
-                type: 'bar'
-            },
-            title: {
-                text: 'Fruit Consumption'
-            },
-            xAxis: {
-                categories: ['Apples', 'Bananas', 'Oranges']
-            },
-            yAxis: {
-                title: {
-                    text: 'Fruit eaten'
-                }
-            },
-            series: [{
-                name: 'Jane',
-                data: [1, 0, 4]
-            }, {
-                name: 'John',
-                data: [5, 7, 3]
-            }]
-        });
-    });
+<script>
+  $(document).foundation();
 </script>
+
+<script>
+$(function () { 
+   $('#container').highcharts({
+    chart: {
+        type: 'line'
+    },
+    title: {
+        text: 'Fruit Consumption'
+    },
+    xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+        title: {
+            text: 'Fruit eaten'
+        }
+    },
+    series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+    }, {
+        name: 'John',
+        data: [5, 7, 3]
+    }]
+  });
+ });
+</script>
+
 </body>
 </html>
