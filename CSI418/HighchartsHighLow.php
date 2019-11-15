3969b78445865cfd53fbc52a3df069799b1c9a62
vincentballarano <vballarano@albany.edu>
2019-11-14 19:50:32 -0500

    highlow chart working

--- CSI418/HighchartsHighLow.php
+++ CSI418/HighchartsHighLow.php
@@ -9,7 +9,6 @@
   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="https://code.highcharts.com/stock/highstock.js"></script>
 <script src="https://code.highcharts.com/stock/modules/data.js"></script>
-<script src="https://code.highcharts.com/stock/modules/drag-panes.js"></script>
 <script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
 
 
@@ -30,7 +29,6 @@
 
 <script>
 var ohlc = [],
-    volume = [],
 	groupingUnits = [[
             'week',                         // unit name
             [1]                             // allowed multiples
@@ -38,8 +36,7 @@ var ohlc = [],
             'month',
             [1, 3, 6]
         ]],
-	seriesCounter = 0,
-	names = ['AAPL'];
+	names = ['MSFT'];
 	
 function createChart() {
 
@@ -58,39 +55,19 @@ function createChart() {
             title: {
                 text: 'OHLC'
             },
-            height: '60%',
+            height: '100%',
             lineWidth: 2,
             resize: {
                 enabled: true
             }
-        }, {
-            labels: {
-                align: 'right',
-                x: -3
-            },
-            title: {
-                text: 'Volume'
-            },
-            top: '65%',
-            height: '35%',
-            offset: 0,
-            lineWidth: 2
-    }],
+        }],
 	tooltip: {
-            split: true
+            split: false
     },
     series: [{
             type: 'candlestick',
-            name: 'AAPL',
+            name: names[0],
             data: ohlc,
-            dataGrouping: {
-                units: groupingUnits
-            }
-        }, {
-            type: 'column',
-            name: 'Volume',
-            data: volume,
-            yAxis: 1,
             dataGrouping: {
                 units: groupingUnits
             }
@@ -98,34 +75,24 @@ function createChart() {
   });
 }
 
-$.each(names, function(i, name) {
 
-  $.getJSON('https://www.quandl.com/api/v1/datasets/WIKI/' + name.toLowerCase() + '.json?auth_token=W8yzMDsJZ_TEcrPjWxGn', function(data) {
-    var newData = [],
-	newData2 = [];
+$.getJSON('https://www.quandl.com/api/v1/datasets/WIKI/' + names[0].toLowerCase() + '.json?auth_token=W8yzMDsJZ_TEcrPjWxGn', function(data) {
+    var newData = [];
 	
     data.data.forEach(function(point) {
 	  newData.push([new Date(point[0]), point[1], point[2], point[3], point[4]]);
-	  newData2.push([new Date(point[0]), point[5]]);
     });
 	
     newData.reverse();	
-	newData2.reverse();
-	//document.write(newData.length - 1);
-	//document.write(newData.length - 1);
 	
 	
 	ohlc = newData;
-	volume = newData2;
-    // As we're loading the data asynchronously, we don't know what order it will arrive. So
-    // we keep a counter and create the chart when all the data is loaded.
-    seriesCounter += 1;
+    
 
-    if (seriesCounter === names.length) {
-      createChart();
-    }
+
+    createChart();
   });
-});
+
 
 </script>
 
